ccf29edce7375e31630b54ea47c8d99ce0b41e9b
Matheus <pgmatheus@hotmail.com>
2023-03-15 15:56:49 -0300

    f

--- test.py
+++ test.py
@@ -1,144 +1,27 @@
-import os
-from diffusers import DiffusionPipeline
+import subprocess
+import multiprocessing
+import time
 
-# if download
-model_id = "stabilityai/stable-diffusion-2-1"
-# os.makedirs(MODEL_CACHE, exist_ok=True)
-#local storage
-model_id = "./diffusers-cache/models--stabilityai--stable-diffusion-2-1/snapshots/36a01dc742066de2e8c91e7cf0b8f6b53ef53da1/"
+def run_command(cmd):
+    import tensorflow as tf
+    import tensorflow_hub as hub
+    import gc
 
-MODEL_CACHE = "diffusers-cache"
-
-pipe = DiffusionPipeline.from_pretrained(
-    model_id,
-    cache_dir=MODEL_CACHE,
-)
-
-import os
-from typing import List
-
-import torch
-from diffusers import (
-    StableDiffusionPipeline,
-    StableDiffusionImg2ImgPipeline,
-    PNDMScheduler,
-    LMSDiscreteScheduler,
-    DDIMScheduler,
-    EulerDiscreteScheduler,
-    EulerAncestralDiscreteScheduler,
-    DPMSolverMultistepScheduler,
-)
-from PIL import Image
-from cog import BasePredictor, Input, Path
-
-MODEL_ID = "stabilityai/stable-diffusion-2-1"
-MODEL_CACHE = "diffusers-cache"
-
-class Predictor(BasePredictor):
-    def setup(self):
-       
-        print("Loading pipeline...")
-        self.txt2img_pipe = StableDiffusionPipeline.from_pretrained(
-            MODEL_ID,
-            cache_dir=MODEL_CACHE,
-            local_files_only=True,
-        ).to("cuda")
-        self.img2img_pipe = StableDiffusionImg2ImgPipeline(
-            vae=self.txt2img_pipe.vae,
-            text_encoder=self.txt2img_pipe.text_encoder,
-            tokenizer=self.txt2img_pipe.tokenizer,
-            unet=self.txt2img_pipe.unet,
-            scheduler=self.txt2img_pipe.scheduler,
-            safety_checker=self.txt2img_pipe.safety_checker,
-            feature_extractor=self.txt2img_pipe.feature_extractor,
-        ).to("cuda")
-
-    @torch.inference_mode()
-    def predict(
-        self,
-        prompt: str = Input(
-            description="Input prompt",
-            default="A fantasy landscape, trending on artstation",
-        ),
-        negative_prompt: str = Input(
-            description="The prompt NOT to guide the image generation. Ignored when not using guidance",
-            default=None,
-        ),
-        image: Path = Input(
-            description="Inital image to generate variations of.",
-        ),
-        width: int = Input(
-            description="Width of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        height: int = Input(
-            description="Height of output image. Maximum size is 1024x768 or 768x1024 because of memory limits",
-            choices=[128, 256, 384, 448, 512, 576, 640, 704, 768, 832, 896, 960, 1024],
-            default=512,
-        ),
-        prompt_strength: float = Input(
-            description="Prompt strength when providing the image. 1.0 corresponds to full destruction of information in init image",
-            default=0.8,
-        ),
-        num_outputs: int = Input(
-            description="Number of images to output. Higher number of outputs may OOM.",
-            ge=1,
-            le=8,
-            default=1,
-        ),
-        num_inference_steps: int = Input(
-            description="Number of denoising steps", ge=1, le=500, default=25
-        ),
-        guidance_scale: float = Input(
-            description="Scale for classifier-free guidance", ge=1, le=20, default=7.5
-        ),
-        scheduler: str = Input(
-            default="DPMSolverMultistep",
-            choices=["DDIM", "K_EULER", "DPMSolverMultistep", "K_EULER_ANCESTRAL", "PNDM", "KLMS"],
-            description="Choose a scheduler.",
-        ),
-        seed: int = Input(
-            description="Random seed. Leave blank to randomize the seed", default=None
-        ),
-    ) -> List[Path]:
-       
-        if seed is None:
-            seed = int.from_bytes(os.urandom(2), "big")
-        print(f"Using seed: {seed}")
-
-        pipe = self.img2img_pipe
-        extra_kwargs = {
-            "image": Image.open(image).convert("RGB"),
-            "strength": prompt_strength,
-        }
-        pipe.scheduler = make_scheduler(scheduler, pipe.scheduler.config)
-
-        generator = torch.Generator("cuda").manual_seed(seed)
-        output = pipe(
-            prompt=[prompt] * num_outputs if prompt is not None else None,
-            guidance_scale=guidance_scale,
-            generator=generator,
-            num_inference_steps=num_inference_steps,
-            **extra_kwargs,
-        )
-
-        output_paths = []
-        for i, sample in enumerate(output.images):
-            output_path = f"/tmp/out-{i}.png"
-            sample.save(output_path)
-            output_paths.append(Path(output_path))
-
-        return output_paths
+    result = subprocess.run(cmd, stdout=subprocess.PIPE, shell=True)
+    physical_devices = tf.config.list_physical_devices('GPU')
+    tf.config.experimental.set_memory_growth(physical_devices[0], True)
+    hub_handle = "./film_1"
+    model = hub.load(hub_handle)
+    print("Hello, world!")
+    print(result.stdout.decode('utf-8'))
+    tf.compat.v1.reset_default_graph()
+    tf.keras.backend.clear_session()
+    gc.collect()
 
 
-def make_scheduler(name, config):
-    return {
-        "PNDM": PNDMScheduler.from_config(config),
-        "KLMS": LMSDiscreteScheduler.from_config(config),
-        "DDIM": DDIMScheduler.from_config(config),
-        "K_EULER": EulerDiscreteScheduler.from_config(config),
-        "K_EULER_ANCESTRAL": EulerAncestralDiscreteScheduler.from_config(config),
-        "DPMSolverMultistep": DPMSolverMultistepScheduler.from_config(config),
-    }[name]
-
+if __name__ == '__main__':
+    #run_command('ls -l')
+    p = multiprocessing.Process(target=run_command, args=('ls -l',))
+    p.start()
+    p.join()
+    
